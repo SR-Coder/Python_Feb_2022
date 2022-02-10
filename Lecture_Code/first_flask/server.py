00163fe84a1814bd84bc864560bbc8d43863e2c6
Jim Reeder <reederje@gmail.com>
2022-02-10 11:23:34 -0800

    Morning Code Push

---
+++ Lecture_Code/first_flask/server.py
@@ -0,0 +1,36 @@
+from distutils.log import debug
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')  # this is what is typed in the the browser http://localhost:5000/
+def hello_world():
+    print("a request came in on '/' !!!")
+    return "Hello World"
+
+@app.route("/say_hello")
+def say_hello():
+    print("sososososos")
+    return 'Hello Friend thanks for stopping by!!'
+
+@app.route("/say_hello/jim")
+def say_hello_jim():
+    print("inside the 'say_hello/jim' method")
+    return ''
+
+@app.route("/say_hello/<string:name>")
+def say_hello_to(name):
+    return f'Hello {name} Thanks for stopping by!!'
+
+@app.route("/say_hello/<string:name>/<int:age>")
+def say_hello_to_age(name, age):
+    return f'Hello {name} Thanks for stopping by!!i am {age} years old!!!'
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
