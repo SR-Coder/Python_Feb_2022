c9123c1b54647a74a026950cd6b42c9ff0a964aa
Jim Reeder <reederje@gmail.com>
2022-03-02 08:53:08 -0800

    daily algos

---
+++ Algos/W4D2.js
@@ -0,0 +1,186 @@
+/* 
+  Recursive Factorial
+
+  Input: integer
+  Output: integer, product of ints from 1 up to given integer
+  
+  If less than zero, treat as zero.
+  Bonus: If not integer, truncate (remove decimals).
+  
+  Experts tell us 0! is 1.
+  
+  rFact(3) = 6 (1*2*3)
+  rFact(6.8) = 720 (1*2*3*4*5*6)
+*/
+
+const num1 = 3;
+const expected1 = 6;
+// Explanation: 1*2*3 = 6
+
+const num2 = 6.8;
+const expected2 = 720;
+// Explanation: 1*2*3*4*5*6 = 720
+
+const num3 = 0;
+const expected3 = 1;
+
+/**
+ * Recursively multiples 1 to the given int.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} n The int to factorial. Treat negatives as zero and
+ *    floor decimals.
+ * @returns {number} The result of !n.
+ */
+function factorial(n) {}
+
+module.exports = { factorial };
+
+/*****************************************************************************/
+
+/**
+ * Recursively multiples 1 to the given int.
+ * - Time: O(n) linear.
+ * - Space: O(n) linear due to the call stack.
+ * @param {number} n The int to factorial. Treat negatives as zero and
+ *    floor decimals.
+ * @returns {number} The result of !n.
+ */
+function factorial(n) {
+  // Termination Condition if it's bad data (not a number)
+  if (isNaN(parseInt(n))) {
+    return null;
+  }
+
+  n = Math.floor(n);
+
+  // Base case
+  if (n <= 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+/* 
+  Return the fibonacci number at the nth position, recursively.
+  
+  Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+  The next number is found by adding up the two numbers before it,
+  starting with 0 and 1 as the first two numbers of the sequence.
+*/
+
+const num1 = 0;
+const expected1 = 0;
+
+const num2 = 1;
+const expected2 = 1;
+
+const num3 = 2;
+const expected3 = 1;
+
+const num4 = 3;
+const expected4 = 2;
+
+const num5 = 4;
+const expected5 = 3;
+
+const num6 = 8;
+const expected6 = 21;
+
+/**
+ * Recursively finds the nth number in the fibonacci sequence.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} num The position of the desired number in the fibonacci sequence.
+ * @returns {number} The fibonacci number at the given position.
+ */
+function fibonacci(num) {}
+
+module.exports = { fibonacci };
+
+/*****************************************************************************/
+
+/**
+ * Naive recursion.
+ * - Time: O(2^n) exponential.
+ * - Space: O(n) linear due to the max call stack depth.
+ */
+function fibNaiveRecursion(n) {
+  if (n < 0) {
+    return null;
+  }
+
+  if (n < 2) {
+    return n;
+  }
+  return fibNaiveRecursion(n - 1) + fibNaiveRecursion(n - 2);
+}
+
+/**
+ * Memoization solution.
+ * In computing, memoization or memoisation is an optimization technique used
+ * primarily to speed up computer programs by storing the results of expensive
+ * function calls and returning the cached result when the same inputs occur
+ * again.
+ * - Time: O(2n) -> O(n) linear.
+ * - Space: O(2n) -> O(n) linear. Call stack depth memo length are both 'n'.
+ */
+function fibonacci(n, memo = { 0: 0, 1: 1 }) {
+  if (n < 0) {
+    return null;
+  }
+
+  if (memo[n] !== undefined) {
+    return memo[n];
+  }
+
+  memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+
+  return memo[n];
+}
+
+/**
+ * Non recursive.
+ * - Time: O(n) linear.
+ * - Space: O(2n) -> O(n) linear. Call stack depth seq length are both 'n'.
+ */
+function fibonacciIterative(n) {
+  if (n < 0) {
+    return null;
+  }
+
+  const seq = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    seq.push(seq[i - 2] + seq[i - 1]);
+  }
+  return seq[n];
+}
+
+/**
+ * Iteratively calculates the nth fib num.
+ * - Time: O(n) linear.
+ * - Space: O(3) -> O(1) constant. There are 3 vars used.
+ * @see https://medium.com/developers-writing/fibonacci-sequence-algorithm-in-javascript-b253dc7e320e
+ * @param {number} n The nth fib num to be calculated
+ * @returns {number} The nth fib num.
+ */
+function fib(n) {
+  if (n < 0) {
+    return null;
+  }
+
+  let prev = 1;
+  let prev2 = 0;
+  let temp;
+
+  while (n > 0) {
+    temp = prev;
+    prev = prev + prev2;
+    prev2 = temp;
+    n--;
+    // console.log("prev:", prev, "prev2:", prev2, "temp:", temp);
+  }
+  return prev2;
+}
